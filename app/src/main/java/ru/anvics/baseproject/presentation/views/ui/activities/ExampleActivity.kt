0a88e332dffe4111b809860455f3763005a50427
Egor Mikhalchenko <gorniygor@gmail.com>
2018-06-25 19:05:22 +0300

    Попытка разделять текст, который распознаётся.

--- app/src/main/java/ru/anvics/baseproject/presentation/views/ui/activities/ExampleActivity.kt
+++ app/src/main/java/ru/anvics/baseproject/presentation/views/ui/activities/ExampleActivity.kt
@@ -1,29 +1,120 @@
 package ru.anvics.baseproject.presentation.views.ui.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.util.Log
+import android.view.SurfaceHolder
 import com.arellomobile.mvp.presenter.InjectPresenter
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.text.TextBlock
+import com.google.android.gms.vision.text.TextRecognizer
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.anvics.baseproject.R
 import ru.anvics.baseproject.domain.ExampleEntity
 import ru.anvics.baseproject.presentation.presenters.ExamplePresenter
 import ru.anvics.baseproject.presentation.views.iviews.ExampleView
+import java.io.IOException
+
 
 class ExampleActivity : BaseActivity(), ExampleView {
 
     @InjectPresenter
     lateinit var presenter: ExamplePresenter
 
+    lateinit var mCameraSource: CameraSource
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnGetEntity.setOnClickListener {
-            presenter.getEntityById(100)
-        }
+        startCameraSource()
     }
 
     override fun setEntity(entity: ExampleEntity) {
-        tvEntityId.text = entity.id.toString()
-        tvEntityTitle.text = entity.title
+        
+//        textView.text = entity.title
+    }
+
+    private fun startCameraSource() {
+
+        //Create the TextRecognizer
+        val textRecognizer = TextRecognizer.Builder(applicationContext).build()
+
+        if (!textRecognizer.isOperational) {
+            Log.w("myTag", "Detector dependencies not loaded yet")
+        } else {
+
+            //Initialize camerasource to use high resolution and set Autofocus on.
+            mCameraSource = CameraSource.Builder(applicationContext, textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setAutoFocusEnabled(true)
+                    .setRequestedFps(2.0f)
+                    .build()
+
+            /**
+             * Add call back to SurfaceView and check if camera permission is granted.
+             * If permission is granted we can start our cameraSource and pass it to surfaceView
+             */
+            surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
+                override fun surfaceCreated(holder: SurfaceHolder) {
+                    try {
+
+                        if (ActivityCompat.checkSelfPermission(applicationContext,
+                                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(this@ExampleActivity,
+                                    arrayOf(Manifest.permission.CAMERA), 1)
+                            return
+                        }
+                        mCameraSource.start(surfaceView.holder)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                }
+
+                override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
+
+                /**
+                 * Release resources for cameraSource
+                 */
+                override fun surfaceDestroyed(holder: SurfaceHolder) {
+                    mCameraSource.stop()
+                }
+            })
+
+            //Set the TextRecognizer's Processor.
+            textRecognizer.setProcessor(object : Detector.Processor<TextBlock> {
+                override fun release() {}
+
+                /**
+                 * Detect all the text from camera using TextBlock and the values into a stringBuilder
+                 * which will then be set to the textView.
+                 */
+                override fun receiveDetections(detections: Detector.Detections<TextBlock>) {
+                    val items = detections.detectedItems
+                    if (items.size() != 0) {
+
+                        textView.post({
+                            val stringBuilder = StringBuilder()
+                            for (i in 0 until items.size()) {
+                                val item = items.valueAt(i)
+                                /*stringBuilder.append("Блок:\n")
+                                item.components.forEach {
+                                    stringBuilder.append(it)
+                                    stringBuilder.append("\n")
+                                }*/
+                                stringBuilder.append(item.value)
+                                stringBuilder.append("\n")
+                            }
+                            textView.text = stringBuilder.toString()
+                        })
+                    }
+                }
+            })
+        }
     }
 }
